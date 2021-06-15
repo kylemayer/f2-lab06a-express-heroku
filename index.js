044f640a2ba3c1d7cc68d05f066ed105857a931c
Kyle Mayer <x@kylemayer.dev>
2021-06-15 14:49:41 -0700

    server js, hello world example test from Express js docs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('something');
+// const app = require('./server.js');
+// const port = process.env.PORT || 777;
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
+
