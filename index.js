def56cac91ad982aa25eb9aa6410dcd74f29c858
Kyle Mayer <x@kylemayer.dev>
2021-06-15 23:32:39 -0700

    nodemon

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server.js');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7777;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
